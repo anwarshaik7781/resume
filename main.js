1bc851ee9b7d966625c49c789a1c0842ea83e80a
anwarshaik7781 <anwarshaik7781@gmail.com>
2018-07-27 16:07:15 +0530

    Updated

--- main.js
+++ main.js
@@ -11,6 +11,45 @@ function loadjson(file,callback) {
 }
 loadjson("data.json",function(text){
 	let data=JSON.parse(text);
-	console.log(data); 
+	console.log(data);
+	basic(data.details); 
 })
 
+var main=document.querySelector(".main");
+var left=document.createElement("div");
+left.classList.add("left");
+main.appendChild(left);
+function basic(basicdetails){
+var image1=document.createElement("img");
+image1.src=basicdetails.image;
+left.appendChild(image1);
+var name=document.createElement("h3");
+name.textContent=basicdetails.name;
+left.appendChild(name);
+var email=document.createElement("h3");
+email.textContent=basicdetails.email;
+left.appendChild(email);
+var phone=document.createElement("h3");
+phone.textContent=basicdetails.phone;
+left.appendChild(phone);
+} 
+
+
+var main=document.querySelector(".main");
+var right=document.createElement("div");
+right.classList.add("right");
+main.appendChild(right);
+function basic(basicdetails){
+var image1=document.createElement("img");
+image1.src=basicdetails.image;
+right.appendChild(image1);
+var name=document.createElement("h3");
+name.textContent=basicdetails.name;
+right.appendChild(name);
+var email=document.createElement("h3");
+email.textContent=basicdetails.email;
+right.appendChild(email);
+var phone=document.createElement("h3");
+phone.textContent=basicdetails.phone;
+right.appendChild(phone);
+}
